cd6c681962249f01ac2bd20a6c7869c030d07eb5
Shriphani Palakodety <shriphanip@gmail.com>
2016-05-19 03:27:23 -0700

    simple tests :)

--- test/fort_knox/core_test.clj
+++ test/fort_knox/core_test.clj
@@ -1,7 +1,30 @@
 (ns fort-knox.core-test
   (:require [clojure.test :refer :all]
-            [fort-knox.core :refer :all]))
+            [fort-knox.core :refer :all]
+            [clojure.core.cache :as cache]
+            [me.raynes.fs :as fs]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest lmdb-cache-test
+  (testing "Start with an empty cache, add a bunch of things ^.^"
+
+    (let [path (fs/temp-name "tmp-cache")
+          the-cache (make-cache path)]
+      (is
+       (not
+        (cache/has? the-cache
+                    "foo")))
+
+
+      (cache/miss the-cache
+                  "foo"
+                  "bar")
+      (is
+       (cache/has? the-cache
+                   "foo"))
+
+      (is
+       (= "bar"
+          (cache/lookup the-cache
+                        "foo")))
+
+      (fs/delete-dir path))))
