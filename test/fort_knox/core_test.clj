f2670f4f0d6dcf6eab3b8552dd9e9bafc9e04403
Shriphani Palakodety <shriphanip@gmail.com>
2016-09-25 17:20:28 -0700

    stuff is core is now moved elsewhere

--- test/fort_knox/core_test.clj
+++ test/fort_knox/core_test.clj
@@ -1,6 +1,6 @@
 (ns fort-knox.core-test
   (:require [clojure.test :refer :all]
-            [fort-knox.lmdb :as lmdb]
+            [fort-knox.core :refer :all]
             [clojure.core.cache :as cache]
             [me.raynes.fs :as fs]))
 
@@ -8,7 +8,32 @@
   (testing "Start with an empty cache, add a bunch of things ^.^"
 
     (let [path (fs/temp-name "tmp-cache")
-          the-cache (lmdb/make-cache path)]
+          the-cache (make-cache path :type :lmdb)]
+      (is
+       (not
+        (cache/has? the-cache
+                    "foo")))
+
+
+      (cache/miss the-cache
+                  "foo"
+                  "bar")
+      (is
+       (cache/has? the-cache
+                   "foo"))
+
+      (is
+       (= "bar"
+          (cache/lookup the-cache
+                        "foo")))
+
+      (fs/delete-dir path))))
+
+(deftest leveldb-cache-test
+  (testing "Start with an empty cache, add a bunch of things ^.^"
+
+    (let [path (fs/temp-name "tmp-cache")
+          the-cache (make-cache path :type :leveldb)]
       (is
        (not
         (cache/has? the-cache
