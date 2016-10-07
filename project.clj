45215a2d2f97250200914032efbdfe00890decf3
Shriphani Palakodety <shriphanip@gmail.com>
2016-10-06 21:48:09 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fort-knox "0.2.0"
+(defproject fort-knox "0.3.0"
   :description "A disk-backed core.cache implementation based on LMDB"
   :url "https://github.com/shriphani/fort-knox"
   :license {:name "Eclipse Public License"
